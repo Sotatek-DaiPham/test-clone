0bd49d806774cff0cb15e61d48c1312d5ee5a1b5
Sotatek-DaiPham <dai.pham@sotatek.com>
2024-10-25 08:37:48 +0700

    feat: add truncate component

---
+++ src/components/app-truncate-text/index.tsx
@@ -0,0 +1,25 @@
+// src/components/app-long-text/TruncatedName.tsx
+
+import AppTooltip from "../app-tooltip";
+
+interface AppTruncateTextProps {
+  text: string;
+  maxLength?: number;
+}
+
+const AppTruncateText: React.FC<AppTruncateTextProps> = ({
+  text,
+  maxLength = 6,
+}) => {
+  return (
+    <>
+      {text && text.length > maxLength ? (
+        <AppTooltip title={text}>{text.slice(0, maxLength)}...</AppTooltip>
+      ) : (
+        text
+      )}
+    </>
+  );
+};
+
+export default AppTruncateText;
