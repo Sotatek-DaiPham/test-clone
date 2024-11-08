1577a0a57353527aca67d68211c132618f716724
Sotatek-TinnNguyen <tinn.nguyen@sotatek.com>
2024-11-09 00:08:20 +0700

    remove /en/rain-pump unused

--- src/app/en/rain-pump/(sidebar-layout)/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React, { ReactNode } from "react";
-import AppLayoutPrimary from "@/components/app-layout/app-layout-primary";
-
-const SidebarLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
-  return <AppLayoutPrimary>{children}</AppLayoutPrimary>;
-};
-
-export default SidebarLayout;
