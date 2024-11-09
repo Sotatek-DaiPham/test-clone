75f2636cdec7281d83c28ba4b41e34e7379d34d9
Sotatek-TinnNguyen <tinn.nguyen@sotatek.com>
2024-11-09 09:42:30 +0700

    remove harded code routing

--- src/constant/router.ts
+++ src/constant/router.ts
@@ -1,10 +1,10 @@
 export const PATH_ROUTER = {
-  DASHBOARD: "/en/rain-pump",
-  MY_PROFILE: "/en/rain-pump/my-profile",
-  NOTIFICATION: "/en/rain-pump/notification",
-  MY_TOKENS: "/en/rain-pump/my-tokens",
-  LEADER_BOARD: "/en/rain-pump/leader-board",
-  TOKEN_DETAIL: (id: any) => `/en/rain-pump/token-detail/${id}`,
-  CREATE_TOKEN: "/en/rain-pump/create-token",
-  USER_PROFILE: (address: any) => `/en/rain-pump/user/${address}`,
+  DASHBOARD: "",
+  MY_PROFILE: "/my-profile",
+  NOTIFICATION: "/notification",
+  MY_TOKENS: "/my-tokens",
+  LEADER_BOARD: "/leader-board",
+  TOKEN_DETAIL: (id: any) => `/token-detail/${id}`,
+  CREATE_TOKEN: "/create-token",
+  USER_PROFILE: (address: any) => `/user/${address}`,
 };
