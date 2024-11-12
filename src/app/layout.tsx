3d5134c45a5122ed51c4ff6ae3d74f0786007d56
Sotatek-DaiPham <dai.pham@sotatek.com>
2024-11-12 12:28:46 +0700

    feat: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body suppressHydrationWarning={true}>
         <ReduxProviders>
           <WagmiRainbowKitProvider>
