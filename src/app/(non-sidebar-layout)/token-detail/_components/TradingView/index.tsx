8a08d293fa2417149e1c2d14a59b76d312707cfe
Sotatek-SyDo <sy.do@sotatek.com>
2024-11-19 10:20:29 +0700

    fix volume precision

--- src/app/(non-sidebar-layout)/token-detail/_components/TradingView/index.tsx
+++ src/app/(non-sidebar-layout)/token-detail/_components/TradingView/index.tsx
@@ -78,6 +78,7 @@ async function getData({ resolution, tokenAddress, endTime }: IDataChart) {
       new BigNumber(bar.volume).div(new BigNumber(10).pow(6))?.toString()
     ),
   }));
+  console.log("bar", bars);
   return bars;
 }
 
@@ -182,7 +183,7 @@ const TradingView = () => {
         has_weekly_and_monthly: true,
         intraday_multipliers: configurationData.intraday_multipliers,
         supported_resolutions: configurationData.supported_resolutions,
-        // volume_precision: volumePrecision,
+        volume_precision: 5,
       };
       onSymbolResolvedCallback(symbolInfo);
     },
