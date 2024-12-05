65d6c9204bc7ad2ffa004e7ce36bc71b475acdc3
Sotatek-DaiPham <dai.pham@sotatek.com>
2024-12-05 23:32:13 +0700

    fix: update gas fee tooltip

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -11,17 +11,17 @@ export const LIMIT_COIN_ITEMS_TABLE = 100;
 
 export const MINIMUM_BUY_AMOUNT = 0.0001;
 
-export const DECIMAL_USDT = 6;
+export const DECIMAL_DISPLAY = 6;
 
 export const NATIVE_TOKEN_DECIMAL = 1e18;
 
 export const TOKEN_DECIMAL = 1e18;
 
-// export const ETH_THRESHOLD = 3.5;
-export const ETH_THRESHOLD = 0.5;
+export const ETH_THRESHOLD = 3.5;
+// export const ETH_THRESHOLD = 0.5;
 
-// export const ETH_THRESHOLD_WITH_FEE = 3.535;
-export const ETH_THRESHOLD_WITH_FEE = 0.505;
+export const ETH_THRESHOLD_WITH_FEE = 3.535;
+// export const ETH_THRESHOLD_WITH_FEE = 0.505;
 
 export const PREDEFINE_AMOUNT = ["0.005", "0.01", "0.05", "0.1", "0.5"];
 
